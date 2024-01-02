3315e57f1874ef49417fd55b55f029947d8054cc
Mehmet Uruş <mehmeturus73@gmail.com>
2024-01-02 12:45:27 +0300

    DialogMetinKutusuWithC#
    C# ile dialog metin kutusu

---
+++ DialogMetinKutusu/Uyg2Dialoglar/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Uyg2Dialoglar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(360, 52);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(806, 538);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 52);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Mesaj Kutusu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(20, 114);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Dosya Aç";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(20, 183);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 35);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Dosyayı Kaydet";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(20, 246);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(159, 35);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Font Değiştir";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(20, 323);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(159, 35);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Renk Değiştir";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(20, 393);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(159, 33);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Resim Aç";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(253, 285);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 73);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ DialogMetinKutusu/Uyg2Dialoglar/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uyg2Dialoglar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult basilan = MessageBox.Show("Bu bir mesaj kutusudur", "Başlık", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error);
+            if (basilan == DialogResult.Yes)
+                MessageBox.Show("Yese bastiniz");
+            // message box 4 parametrreli aslında  MessageBox.Show("Yese bastiniz","başlık buda ", MessageBoxButtons. vs durdur yeniden dene tamam vs,Message.BoxIcon.errror vs ikon simge buda );
+            else if (basilan == DialogResult.No)
+                MessageBox.Show("Noya bastiniz");
+            else
+                MessageBox.Show("Cancela bastiniz");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            od.Filter = "Metin Dosyaları|*.txt;*.doc;*.docx";
+            DialogResult basilan = od.ShowDialog();
+            //Show dialog KESİN YAP gösterilmesi için, bunu toolbaxdan diaglogdan da ekleyebilrdin aşağıda gösterir ama
+            if (basilan == DialogResult.OK)
+            {
+                //secilen dosyayi al
+                string secilen = od.FileName;
+                // richTextBox1.LoadFile(secilen, RichTextBoxStreamType.PlainText);
+                System.IO.StreamReader on = new System.IO.StreamReader(secilen);
+                string icerik = on.ReadToEnd();
+                richTextBox1.Text = icerik;
+                on.Close();
+
+                //richtextboxta görüntüle
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sd = new SaveFileDialog();
+            sd.Filter = "Metin Dosyaları|*.txt;*.pdf"; //arada ; ile birdden f
+            sd.DefaultExt = ".txt";
+            DialogResult basilan = sd.ShowDialog();
+            if (basilan == DialogResult.OK)
+            {
+                string kaydedilecek = sd.FileName;
+                //richTextBox1.SaveFile(kaydedilecek,RichTextBoxStreamType.PlainText);
+                System.IO.StreamWriter yn = new System.IO.StreamWriter(kaydedilecek, false);
+                yn.WriteLine(richTextBox1.Text);
+                yn.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FontDialog fd = new FontDialog();
+            DialogResult basilan = fd.ShowDialog();
+            if (basilan == DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = fd.Font;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            DialogResult basilan = cd.ShowDialog();
+            if (basilan == DialogResult.OK)
+            {
+                richTextBox1.SelectionColor = cd.Color;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            od.Filter = "Resim Dosyaları|*.jpeg;*.png;";
+            DialogResult basilan = od.ShowDialog();
+
+            if (basilan == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(od.FileName);
+
+            }
+
+
+        } //hoca resim ekle şeyinide yaptı onabak son 5dk da
+    }
+    }
